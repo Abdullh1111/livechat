8973fb2d10a0e842a753af64eec6a7ecdabfd582
Abdullh1111 <138347870+Abdullh1111@users.noreply.github.com>
2024-08-26 09:54:14 +0600

    Update Url.ts

--- frontend/src/Mainurl/Url.ts
+++ frontend/src/Mainurl/Url.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-export const url = "https://livechat-backend-h4zd.onrender.com/api/v1";
+export const url = "https://livechat-backend-h4zd.onrender.com:3000/api/v1";
 
-export const socket = io("https://livechat-backend-h4zd.onrender.com");
+export const socket = io("https://livechat-backend-h4zd.onrender.com:3000");
 
