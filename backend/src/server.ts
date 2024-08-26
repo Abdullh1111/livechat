8169326b867201f95996f9ebebb7a1e94617feed
Abdullh1111 <138347870+Abdullh1111@users.noreply.github.com>
2024-08-26 14:53:55 +0600

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,7 +21,7 @@ async function main() {
   const io = new Server(server, {
     pingTimeout: 600000,
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://livechat-frontend-i0by.onrender.com",
       credentials: true,
     },
   });
